e9973b72fac014ca58ef5d7c3dfdf2ef491271bd
john <johnsantosh2@gmail.com>
2023-05-20 17:33:17 +0530

    data.js

--- src/data.js
+++ src/data.js
@@ -1,21 +1,69 @@
+import {
+  FaHome,
+  FaRegUser,
+  FaBookOpen,
+  FaLinkedin,
+  FaWhatsapp,
+} from "react-icons/fa";
+import { BiMessageRounded, BiGitBranch } from "react-icons/bi";
 import { nanoid } from "nanoid";
-import { FaHome, FaRegUser, FaBookOpen } from "react-icons/fa";
-import { BiMessageRounded, BiMoveHorizontal } from "react-icons/bi";
 import { CgRadioChecked } from "react-icons/cg";
-
+import { RiMailStarLine, RiMessengerLine } from "react-icons/ri";
+import { AiFillGithub } from "react-icons/ai";
 import { BsFillClipboardCheckFill } from "react-icons/bs";
+
 export const social = [
-  { id: nanoid(), FaHome, url: "#" },
-  { id: nanoid(), FaHome, url: "#" },
-  { id: nanoid(), FaHome, url: "#" },
+  {
+    id: nanoid(),
+    Icon: BiGitBranch,
+    url: "https://github.com/John-santhosh/React-portfolio-v1",
+  },
+  {
+    id: nanoid(),
+    Icon: FaLinkedin,
+    url: "https://www.linkedin.com/in/john-santhosh-664539219",
+  },
+  { id: nanoid(), Icon: AiFillGithub, url: "https://github.com/John-santhosh" },
 ];
 
 export const navLinks = [
-  { id: 1, Icon: FaHome, url: "#" },
-  { id: 2, Icon: FaRegUser, url: "#about" },
-  { id: 3, Icon: FaBookOpen, url: "#skills" },
-  { id: 4, Icon: BsFillClipboardCheckFill, url: "#portfolio" },
-  { id: 5, Icon: BiMessageRounded, url: "#contact" },
+  { id: 1, Icon: FaHome, url: "#", text: "home" },
+  { id: 2, Icon: FaRegUser, url: "#about", text: "about" },
+  { id: 3, Icon: FaBookOpen, url: "#skills", text: "skills" },
+  {
+    id: 4,
+    Icon: BsFillClipboardCheckFill,
+    url: "#portfolio",
+    text: "portfolio",
+  },
+  { id: 5, Icon: BiMessageRounded, url: "#contact", text: "contact" },
+];
+
+export const contacts = [
+  {
+    id: nanoid(),
+    Icon: RiMailStarLine,
+    text: "Send a mail",
+    connect: "Email",
+    connectId: "johnsantosh@gmail.com",
+    url: "mailto:johnsantosh2@gamil.com",
+  },
+  {
+    id: nanoid(),
+    Icon: RiMessengerLine,
+    text: "Send a message",
+    connect: "Messenger",
+    connectId: "john.santosh",
+    url: "https://m.me/john.santosh.50",
+  },
+  {
+    id: nanoid(),
+    Icon: FaWhatsapp,
+    text: "Send a message",
+    connect: "Whatsapp",
+    connectId: "75501883XX",
+    url: "https://Wa.me/7550188335",
+  },
 ];
 
 export const skills = {
